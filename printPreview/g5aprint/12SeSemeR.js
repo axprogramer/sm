1f0a3983a7b52729e559a58c98e265aaf4134bd5
axprogramer <105089786+axprogramer@users.noreply.github.com>
2023-07-03 14:24:23 +0700

    update

--- printPreview/g5aprint/12SeSemeR.js
+++ printPreview/g5aprint/12SeSemeR.js
@@ -315,7 +315,7 @@ function myNov() {
   document.body.innerHTML = newstr;
   window.print();
   document.body.innerHTML = oldstr;
-  window.location.reload();
+  // window.location.reload();
   return false;
 }
 function addClassNov() {
